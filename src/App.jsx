a5ec936e04686fb287799360723707f07bd2b269
EvilFlame112 <rampranav25@gmail.com>
2024-08-13 13:24:33 +0530

    Update todolist with input and add

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
+import { useState } from "react"
 import TodoInput from "./components/TodoInput"
 import TodoList from "./components/TodoList"
 
 function App() {
 
+  const [todos, setTodos] = useState([
+    'Go to the gym',
+    'Drink 3l of water'
+  ])
+
+  function handleAddTodos(newTodo) {
+    const newTodoList = [...todos, newTodo]
+    setTodos(newTodoList)
+  }
+
   return (
-    <main>
-      <TodoInput />
-      <TodoList />
-    </main>
+    <>
+      <TodoInput handleAddTodos={handleAddTodos}/>
+      <TodoList todos={todos}/>
+    </>
   )
 }
 
