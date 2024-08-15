33da4c683997685d5b5a3dc5476d881d50b95227
EvilFlame112 <rampranav25@gmail.com>
2024-08-15 12:19:08 +0530

    complete basic structure

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 
 export default function TodoCard(props) {
-  const { children } = props
+  const { children, handleDeleteTodo, handleEditTodo, index } = props
   return (
     <li className='todoItem'>
       {children}
       <div className='actionsContainer'>
-        <i className="fa-regular fa-pen-to-square"></i>
-        <i class="fa-solid fa-trash"></i>
+        <button onClick={() => {handleEditTodo(index)}}>
+          <i className="fa-regular fa-pen-to-square"></i>
+        </button>
+        <button onClick={() => {handleDeleteTodo(index)}}>
+          <i class="fa-solid fa-trash"></i>
+        </button>
       </div>
     </li>
   )
